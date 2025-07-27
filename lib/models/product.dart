ad3c71ae9de0ab0b5b7fc758ad0393da3f58f990
Kaletsidik Ayalew <alexkalalw@gmail.com>
2025-07-27 10:51:12 +0300

    add the lib/models directory for code reusbility and origaniztion

---
+++ lib/models/product.dart
@@ -0,0 +1,15 @@
+class Product {
+  final String name;
+  final String category;
+  final double price;
+  final double rating;
+  final String imagePath;
+
+  Product({
+    required this.name,
+    required this.category,
+    required this.price,
+    required this.rating,
+    required this.imagePath,
+  });
+}
